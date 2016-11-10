e718c81fcfcee9fb8798f8d4be0499d44ebcbe22
omsk16 <kenteg91@gmail.com>
2016-11-10 09:15:56 +0600

    fix import

--- src/com/luxoft/bankapp/service/ClientRegistrationListener.java
+++ src/com/luxoft/bankapp/service/ClientRegistrationListener.java
@@ -1,10 +1,11 @@
 package com.luxoft.bankapp.service;
 
-import com.luxoft.bankapp.domain.Client;
+import com.luxoft.bankapp.model.Client;
 
 /**
  * Created by omsk16 on 11/3/2016.
  */
+
 public interface ClientRegistrationListener {
-    public void OnClientAdded(Client c);
+    void OnClientAdded(Client c);
 }
