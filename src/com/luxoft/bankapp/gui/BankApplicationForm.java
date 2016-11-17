d88c3542614f7b1d28ec9278122d951394829aff
kenteg <kenteg91@gmail.com>
2016-11-17 23:26:24 +0600

    created gui (empty form)

---
+++ src/com/luxoft/bankapp/gui/BankApplicationForm.java
@@ -0,0 +1,13 @@
+package com.luxoft.bankapp.gui;
+
+import javax.swing.*;
+
+/**
+ * @author Khrishpens Viktor
+ *         created Ноябрь 17 2016
+ */
+public class BankApplicationForm {
+    private JTable table1;
+    private JPanel panel1;
+    private JButton button1;
+}
